b0d465874859616c6678303447fc36794988516f
sakitam-fdd <smilefdd@gmail.com>
2023-04-23 10:00:13 +0800

    fix: ci

--- playground/vue3/vite.config.ts
+++ playground/vue3/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({ command, mode }) => ({
+  base: command === 'build' ? './' : '/',
   plugins: [vue()],
   publicDir: 'public',
   // server: {
